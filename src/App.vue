65549cbef7495cc4b918b6596e473bac70d7cdf0
Ilya Golovin <me@ilscc.ru>
2023-02-01 12:51:20 +0300

    feat: setup working env

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,31 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="test">{{ counter.count }}</div>
   </div>
 </template>
 
 <script>
+import { defineStore } from "pinia";
 import HelloWorld from "./components/HelloWorld.vue";
-
+const useCounterStore = defineStore("counter", {
+  state: () => ({ count: 0 }),
+  actions: {
+    increment() {
+      this.count += 1;
+    },
+  },
+});
 export default {
   name: "App",
   components: {
     HelloWorld,
+  },
+  setup() {
+    const counter = useCounterStore();
+    return {
+      counter,
+    };
   },
 };
 </script>
@@ -24,5 +39,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.test {
+  width: 200px;
+  height: 200px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        pinia$: path.resolve(__dirname, "node_modules/pinia/index.js"),
+      },
+    },
+  },
 });
